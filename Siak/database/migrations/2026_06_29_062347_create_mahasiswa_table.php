37853c55a3a64e511ae127793c502da5e82f45d3
azizarbiansyah-byte <azizarbiansyah6@gmail.com>
2026-06-29 13:32:52 +0700

    Setup project, add migration and models

---
+++ Siak/database/migrations/2026_06_29_062347_create_mahasiswa_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */ 
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->char('npm', 10);
+            $table->char('nidn', 10)->nullable();
+            $table->string('nama', 50);
+            $table->timestamps();
+
+            $table->primary('npm');
+            $table->foreign('nidn')->references('nidn')->on('dosen')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->dropForeign(['nidn']);
+        });
+        Schema::dropIfExists('mahasiswa');
+    }
+};
